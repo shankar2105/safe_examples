64ba02e11ba3bc4a3122242669479e048a01967a
Shankar <shankar21mail@gmail.com>
2017-03-20 16:12:46 +0530

    Implemented code to support latest APIs

--- markdown_editor/src/store.js
+++ markdown_editor/src/store.js
@@ -1,110 +1,168 @@
 /* global btoa, safeAuth, safeNFS safeCipherOpts, safeStructuredData, safeDataId */
 
 // this is only file directly interacting with SAFE
+import crypto from 'crypto';
+import { APP_ID, APP_VERSION, APP_INFO, CONTAINERS, TYPE_TAG } from './config.js'
 
-import { APP_ID, APP_NAME, APP_VERSION } from './config.js'
+const requiredWindowObj = [
+  'safeApp',
+  'safeMutableData',
+  'safeMutableDataEntries',
+  'safeImmutableData',
+  'safeMutableDataPermissionsSet',
+  'safeMutableDataPermissions',
+  'safeNfs'
+];
 
-if (process.env.NODE_ENV !== 'production') {
-  require('safe-js/dist/polyfill')
-}
+requiredWindowObj.forEach((obj) => {
+  if (!window.hasOwnProperty(obj)) {
+    throw new Error(`${obj} not found. Please check beaker-plugin-safe-app`);
+  }
+});
+
+const INDEX_FILE_NAME = crypto.createHash('sha256').update(`${window.location.host}-${APP_ID}`).digest('hex');
+const RES_URI_KEY = 'SAFE_RES_URI';
 
 // global access state
 let ACCESS_TOKEN;
-let SYMETRIC_CYPHER_HANDLE;
-let INDEX_HANDLE;
 let FILE_INDEX;
-let USER_PREFIX;
 
-// legacy style fallback
-const extractHandle = (res) => res.hasOwnProperty('handleId') ? res.handleId : res.__parsedResponseBody__.handleId;
-
-const _createRandomUserPrefix = () => {
-  let randomString = '';
-  for (var i = 0; i < 10; i++) {
-    // and ten random ascii chars
-    randomString += String.fromCharCode(Math.floor(Math.random(100) * 100));
+const _saveResponseUri = (uri) => {
+  if (typeof uri !== 'string') {
+    throw new Error('URI is not a String');
   }
-  return btoa(`${APP_ID}@${APP_VERSION}#${(new Date()).getTime()}-${randomString}`);
+  window.localStorage.setItem(RES_URI_KEY, uri);
 };
 
-const _refreshCypherHandle = () => {
-  return safeCipherOpts.getHandle(ACCESS_TOKEN,
-    window.safeCipherOpts.getEncryptionTypes().SYMMETRIC)
-    .then(res => {
-        SYMETRIC_CYPHER_HANDLE = extractHandle(res);
-        return SYMETRIC_CYPHER_HANDLE;
+const _getResponseUri = () => {
+  return window.localStorage.getItem(RES_URI_KEY);
+};
+
+const _connectAuthorised = (token, resUri) => {
+  return window.safeApp.connectAuthorised(token, resUri)
+    .then((token) => (ACCESS_TOKEN = token));
+};
+
+const _getBufferedFileIndex = () => {
+  if (!FILE_INDEX) {
+    throw new Error('FILE INDEX is not an Object');
+  }
+  return new Buffer(JSON.stringify(FILE_INDEX)).toString('base64');
+};
+
+const _fetchAccessInfo = () => {
+  return window.safeApp.canAccessContainer(ACCESS_TOKEN, '_public')
+    .then((hasAccess) => {
+      if (!hasAccess) {
+        throw new Error('Cannot access PUBLIC Container');
       }
-    );
+      return true;
+    });
 };
 
-const _refreshConfig = () => {
-  // reading the config from NFS or create it if not yet existing.
-  const FILE_NAME = 'app_config.json';
-  return safeNFS.createFile(ACCESS_TOKEN,
-    // try to create instead then
-    FILE_NAME,
-    JSON.stringify({ 'user_prefix': _createRandomUserPrefix() }), 'application/json')
-    .catch(err => err.errorCode === -505 ? '' : console.error(err)) // ignore file already exists
-    .then(() => safeNFS.getFile(ACCESS_TOKEN, FILE_NAME))
-    .then(resp => resp.json ? resp.json() : JSON.parse(resp.toString()))
-    .then(config => (USER_PREFIX = config.user_prefix));
-};
+const _createMdata = () => {
+  FILE_INDEX = {};
+  return window.safeMutableData.newRandomPrivate(ACCESS_TOKEN, TYPE_TAG)
+    .then((mdata) => {
+      let permSetHandle = null;
+      let pubSignKeyHandle = null;
+      let permHandle = null;
 
-const getSDHandle = (filename) => {
-  let dataIdHandle = null;
-  return safeDataId.getStructuredDataHandle(ACCESS_TOKEN, btoa(`${USER_PREFIX}:${filename}`), 501)
-    .then(extractHandle)
-    .then(handleId => (dataIdHandle = handleId))
-    .then(() => safeStructuredData.getHandle(ACCESS_TOKEN, dataIdHandle))
-    .then(extractHandle)
-    .then(handleId => {
-      safeDataId.dropHandle(ACCESS_TOKEN, dataIdHandle);
-      return handleId;
+      return window.safeMutableData.newPermissionSet()
+        .then((permSet) => (permSetHandle = permSet))
+        .then(() => window.safeMutableDataPermissionsSet.setAllow(ACCESS_TOKEN, permSetHandle, 'Insert'))
+        .then(() => window.safeMutableDataPermissionsSet.setAllow(ACCESS_TOKEN, permSetHandle, 'Update'))
+        .then(() => window.safeMutableDataPermissionsSet.setAllow(ACCESS_TOKEN, permSetHandle, 'Delete'))
+        .then(() => window.safeMutableDataPermissionsSet.setAllow(ACCESS_TOKEN, permSetHandle, 'ManagePermissions'))
+        .then(() => window.safeApp.getPubSignKey())
+        .then((signKey) => (pubSignKeyHandle = signKey))
+        .then(() => window.safeMutableData.newPermissions())
+        .then((perm) => (permHandle = perm))
+        .then(() => window.safeMutableDataPermissions.insertPermissionsSet(ACCESS_TOKEN, permHandle, pubSignKeyHandle, permSetHandle))
+        .then(() => window.safeMutableData.newEntries(ACCESS_TOKEN))
+        .then((entriesHandle) => window.safeMutableDataEntries.insert(ACCESS_TOKEN, entriesHandle, 'FILE_INDEX', _getBufferedFileIndex())
+          .then(() => window.safeMutableData.put(ACCESS_TOKEN, mdata, permHandle, entriesHandle)))
+        .then(() => window.safeMutableData.getNameAndTag(ACCESS_TOKEN, mdata));
     })
-};
-
-const updateFile = (filename, payload) => {
-  let handleId = null;
-  return getSDHandle(filename)
-    .then(sdHandleId => (handleId = sdHandleId))
-    .then(() => safeStructuredData.updateData(ACCESS_TOKEN, handleId, payload, SYMETRIC_CYPHER_HANDLE))
-    .then(() => safeStructuredData.post(ACCESS_TOKEN, handleId))
+    .then((mdataInfo) => {
+      return window.safeApp.getContainer(ACCESS_TOKEN, '_public')
+        .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata)
+          .then((entries) => window.safeMutableDataEntries.mutate(ACCESS_TOKEN, entries)
+            .then((mut) => window.safeMutableDataMutation.insert(ACCESS_TOKEN, mut, INDEX_FILE_NAME, mdataInfo.name)
+              .then(() => window.safeMutableData.applyEntriesMutation(ACCESS_TOKEN, mdata, mut)))));
+    });
 };
 
 export const authorise = () => {
   if (ACCESS_TOKEN) return Promise.resolve(ACCESS_TOKEN);
 
-  return safeAuth.authorise({
-      'name': APP_NAME,
-      'id': APP_ID,
-      'version': APP_VERSION,
-      'vendor': 'MaidSafe Ltd.',
-      'permissions': ['LOW_LEVEL_API']
-    },
-    APP_ID)
-    .then(res => res.__parsedResponseBody__ || res) // legacy style fallback
-    .then(auth => auth.token === APP_ID ? safeAuth.getAuthToken(APP_ID) : auth.token)
-    .then(token => {
-        if (!token) {
-          alert("Authentication failed");
-          throw Error("Authentication Failed");
-        }
-        ACCESS_TOKEN = token;
-        // then fetch a fresh cypherHandle
-        return Promise.all([
-          _refreshCypherHandle(),
-          _refreshConfig()
-        ]).then(() => ACCESS_TOKEN)
+  const responseUri = _getResponseUri();
+
+  return window.safeApp.initialise(APP_INFO)
+    .then((token) => {
+      if (responseUri) {
+        return _connectAuthorised(token, responseUri);
       }
-    );
+      return window.safeApp.authorise(token, CONTAINERS)
+        .then((resUri) => {
+          _saveResponseUri(resUri);
+          return _connectAuthorised(token, resUri);
+        });
+    })
+    .then(() => _fetchAccessInfo());
 };
 
-const _putFileIndex = () => {
-  return safeStructuredData.updateData(ACCESS_TOKEN,
-    INDEX_HANDLE,
-    new Buffer(JSON.stringify(FILE_INDEX)).toString('base64'),
-    SYMETRIC_CYPHER_HANDLE)
-    .then(() => safeStructuredData.post(ACCESS_TOKEN, INDEX_HANDLE));
+export const getFileIndex = () => {
+  if (FILE_INDEX) return Promise.resolve(FILE_INDEX);
+
+  return window.safeApp.getContainer(ACCESS_TOKEN, '_public')
+    .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata))
+    .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, INDEX_FILE_NAME)
+      .then((value) => window.safeMutableData.newPrivate(ACCESS_TOKEN, value.buf, TYPE_TAG)
+        .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata))
+        .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, 'FILE_INDEX'))
+        .then((fileIndex) => {
+          const parsedFileIndex = JSON.parse(new Buffer(fileIndex, 'base64').toString());
+          FILE_INDEX = parsedFileIndex;
+          return FILE_INDEX;
+        }))
+      .catch(() => {
+        console.warn('Creating new record');
+        return _createMdata();
+      }));
+};
+
+// TODO fetch versioned data
+export const readFile = (filename, version) => {
+  return window.safeApp.getContainer(ACCESS_TOKEN, '_public')
+    .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata))
+    .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, INDEX_FILE_NAME)
+      .then((value) => window.safeMutableData.newPrivate(ACCESS_TOKEN, value.buf, TYPE_TAG)
+        .then((mdata) => window.safeMutableData.emulateAs('NFS'))
+        .then((nfs) => {
+          return window.safeNfs.fetch(ACCESS_TOKEN, nfs, filename)
+            .then((file) => window.safeImmutableData.fetch(ACCESS_TOKEN, window.safeNfs.getFileMeta(file).dataMapName))
+            .then((immut) => window.safeImmutableData.read(ACCESS_TOKEN, immut))
+            .then((data) => new Buffer(data, 'base64').toString())
+        })));
+};
+
+export const getSDVersions = (filename) => {
+  return Promise.resolve([]);
+};
+
+const _updateFile = (filename, payload) => {
+  return window.safeApp.getContainer(ACCESS_TOKEN, '_public')
+    .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata))
+    .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, INDEX_FILE_NAME)
+      .then((value) => window.safeMutableData.newPrivate(ACCESS_TOKEN, value.buf, TYPE_TAG)
+        .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata)
+          .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, INDEX_FILE_NAME))
+          .then((value) => {
+            const nfs = window.safeMutableData.emulateAs('NFS');
+            return window.safeNfs.create(ACCESS_TOKEN, nfs, payload)
+              .then((file) => window.safeNfs.update(ACCESS_TOKEN, nfs, file, filename, parseInt(value.version, 10) + 1))
+          }))));
 };
 
 export const saveFile = (filename, data) => {
@@ -116,84 +174,16 @@ export const saveFile = (filename, data) => {
   if (FILE_INDEX[filename]) {
     // this was an edit, add new version
     console.log("existing");
-    return updateFile(filename, payload);
+    return _updateFile(filename, payload);
   } else {
-    // file is being created for the first time
-    return safeStructuredData.create(ACCESS_TOKEN,
-      // trying to come up with a name that is super unlikely to clash ever.
-      btoa(`${USER_PREFIX}:${filename}`),
-      // 501 => we want this versioned
-      501, payload, SYMETRIC_CYPHER_HANDLE)
-      .then(extractHandle)
-      // save the structure
-      .then(handle => safeStructuredData.put(ACCESS_TOKEN, handle)
-        // fetch a permanent reference
-          .then(() => safeStructuredData.getDataIdHandle(ACCESS_TOKEN, handle))
-          // add the reference to the file index
-          .then((dataHandleId) => {
-            FILE_INDEX[filename] = dataHandleId;
-            return _putFileIndex()
-          })
-      )
+    return window.safeApp.getContainer(ACCESS_TOKEN, '_public')
+      .then((mdata) => window.safeMutableData.getEntries(ACCESS_TOKEN, mdata))
+      .then((entries) => window.safeMutableDataEntries.get(ACCESS_TOKEN, entries, INDEX_FILE_NAME)
+        .then((value) => window.safeMutableData.newPrivate(ACCESS_TOKEN, value.buf, TYPE_TAG)
+          .then((mdata) => window.safeMutableData.emulateAs('NFS'))
+          .then((nfs) => {
+            return window.safeNfs.create(ACCESS_TOKEN, nfs, payload)
+              .then((file) => window.safeNfs.insert(ACCESS_TOKEN, nfs, file, filename))
+          })));
   }
-};
-
-export const getFileIndex = () => {
-  if (FILE_INDEX) return Promise.resolve(FILE_INDEX);
-  const INDEX_FILE_NAME = btoa(`${USER_PREFIX}#index`);
-
-  return safeDataId.getStructuredDataHandle(ACCESS_TOKEN, INDEX_FILE_NAME, 500)
-    .then(extractHandle)
-    .then(handle => safeStructuredData.getHandle(ACCESS_TOKEN, handle)
-      .then(extractHandle)
-      // drop data Handle
-      // .then(sdHandle => safeDataId.dropHandle(handle).then(() => sdHandle))
-      .then(sdHandle => {
-          // store the handle for future reference
-          INDEX_HANDLE = sdHandle;
-          // let's try to read
-          return safeStructuredData.readData(ACCESS_TOKEN, sdHandle, '')
-            .then(resp => resp.json ? resp.json() : JSON.parse(new Buffer(resp).toString()))
-        },
-        (e) => {
-          console.error(e);
-          FILE_INDEX = {};
-          return safeStructuredData.create(ACCESS_TOKEN, INDEX_FILE_NAME, 500,
-            new Buffer(JSON.stringify({})).toString('base64'), SYMETRIC_CYPHER_HANDLE)
-            .then(extractHandle)
-            .then(handle => (INDEX_HANDLE = handle))
-            .then(() => safeStructuredData.put(ACCESS_TOKEN, INDEX_HANDLE)
-              // don't forget to clean up that handle
-              //   .then(() => safeStructuredData.dropHandle(handle))
-            )
-            // and return empty data as payload
-            .then(() => {
-              return {}
-            })
-        }))
-    .then(payload => {
-      // store payload for future reference
-      FILE_INDEX = payload;
-      return FILE_INDEX;
-    });
-};
-
-export const readFile = (filename, version) => {
-  return getSDHandle(filename)
-    .then(handleId => safeStructuredData.readData(ACCESS_TOKEN, handleId, version));
-};
-
-export const getSDVersions = (filename) => {
-  let sdHandleId = null;
-  return getSDHandle(filename)
-    .then(handleId => (sdHandleId = handleId))
-    .then(() => safeStructuredData.getMetadata(ACCESS_TOKEN, sdHandleId))
-    .then(res => res.hasOwnProperty('version') ? res.version : res.__parsedResponseBody__.version)
-    .then(sdVersion => {
-      const iterator = [];
-      for (let i = 0; i <= sdVersion; i++) {
-        iterator.push(i);
-      }
-      return Promise.all(iterator.map(version => safeStructuredData.readData(ACCESS_TOKEN, sdHandleId, version)));
-    });
 };
