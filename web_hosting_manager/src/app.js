3ac9df231f0e014abcca060f82e2f0e417329e1f
Shankar <shankar21mail@gmail.com>
2017-10-09 11:06:55 +0530

    initial commit

---
+++ web_hosting_manager/src/app.js
@@ -0,0 +1,44 @@
+import { remote, ipcRenderer as ipc } from 'electron';
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { loadTranslations, setLocale, syncTranslationWithStore } from 'react-redux-i18n';
+
+import Root from './containers/Root';
+import { configureStore, history } from './store/configureStore';
+import loadLocale from './locales/loader';
+import { initTempFolder } from './lib/temp';
+
+import { receiveResponse } from './actions/authorisation';
+
+const store = configureStore();
+
+initTempFolder();
+
+// setup i18n
+let locale = remote.app.getLocale();
+let translationConfig = loadLocale(locale);
+if (!translationConfig) {
+  locale = 'en';
+  translationConfig = loadLocale(locale);
+}
+syncTranslationWithStore(store);
+store.dispatch(loadTranslations(translationConfig));
+store.dispatch(setLocale(locale));
+
+// handle auth response
+ipc.on('auth-response', (event, response) => {
+  store.dispatch(receiveResponse(response));
+});
+
+const renderApp = () => {
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('App')
+  );
+} 
+renderApp();
+
+if (module.hot) { module.hot.accept(renderApp); }
